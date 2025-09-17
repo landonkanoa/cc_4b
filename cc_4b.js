d4bd703afe069e7c4d644f2b9ede185d29feea36
Landon <landonlopez@usf.edu>
2025-09-17 17:52:42 -0400

    first commit

---
+++ cc_4b.js
@@ -0,0 +1,58 @@
+let product = [
+    {
+        sku: "F-001",
+        name: "strawberry",
+        category: "fruit",
+        price: 10,
+        inventory: 20
+    },
+    {
+        sku: "V-002",
+        name: "lettuce",
+        category: "vegetable",
+        price: 6,
+        inventory: 30
+    },
+    {
+        sku: "F-003",
+        name: "blueberry",
+        category: "fruit",
+        price: 8,
+        inventory: 25
+    },
+    {
+        sku: 'V-004',
+        name: "asparagus",
+        category: "vegetable",
+        price: 4,
+        inventory: 40 
+    },
+    {
+        sku: "D-005",
+        name: "milk",
+        category: "dairy",
+        price: 20,
+        inventory: 50
+    }
+];
+for (let product in product) {
+    let discount
+
+    switch (product.category) {
+        case "fruit":
+            discount = 0.05;
+            break;
+        case "vegetable":
+            discount = 0.1;
+            break;
+        case "dairy":
+            discount = 0.1;
+            break;
+            default: 0;
+                discount = 0;
+    };
+    let promoPrice = product.price - (product.price * discount);
+    product.promoPrice = promoPrice
+}
+
+console.log(product)
